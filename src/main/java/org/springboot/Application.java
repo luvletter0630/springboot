214d58e50059c9aa8a14ec58ea8d88ab6e1a4a11
liwj <761644814@qq.com>
2017-10-16 17:14:02 +0800

    springboot study demo 1

---
+++ src/main/java/org/springboot/Application.java
@@ -0,0 +1,16 @@
+package org.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Created by 76164 on 2017/10/11.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void  main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
